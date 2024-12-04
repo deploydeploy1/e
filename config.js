0ee018cfb702a8429d290a894fe2ecc00bef8b7c
sl-nethu-max <nethumax855@gmail.com>
2024-12-04 18:55:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || '2NEQBJgR#wAhZrO3Foaybk8_dqHAUQmJHcPWpUTCcTNNpY4HP-hE', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',    // Enter Your MongoDB URL
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'sl-nethu-max': process.env.GITHUB_USERNAME,
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
 };
+
